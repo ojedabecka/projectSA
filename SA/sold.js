d27895f65944ba9bafece428de4a5df60240c9fb
Luan <48784874+anluuu@users.noreply.github.com>
2019-06-01 19:39:35 -0300

    Add files via upload

--- SA/sold.js
+++ SA/sold.js
@@ -1,4 +1,3 @@
-
 localStorage.nome;
 localStorage.email;
 localStorage.telefone;
@@ -19,6 +18,11 @@ function salvarInfo(){
     var e = document.getElementById("email").value;
     var t = document.getElementById("telefone").value;
     var s = document.getElementById("site").value;
+
+    if(n ==="" || e === "" || t === "" || s === ""){
+        alert("Informações insuficientes");
+        return;
+    }
 
     localStorage.setItem("nome", n); //localStorage.nome = n;
     localStorage.setItem("email", e); // localStorage.email = e;
@@ -43,18 +47,22 @@ function imprimirInfo(){
     var n = document.getElementById("nome").value;
     var e = document.getElementById("email").value;
     var t = document.getElementById("telefone").value;
+    var s = document.getElementById("site").value;
 
     localStorage.setItem("nome", n); //localStorage.nome = n;
     localStorage.setItem("email", e); // localStorage.email = e;
     localStorage.setItem("telefone", t); // localStorage.telefone = t;
+    localStorage.setItem("site", s);
 
     auxNomeArray.push(n);
     auxEmailArray.push(e);
     auxTelefoneArray.push(t);
+    auxSiteArray.push(s);
 
     localStorage.setItem("nomeArray", auxNomeArray);
     localStorage.setItem("emailArray", auxEmailArray);
     localStorage.setItem("telefoneArray", auxTelefoneArray);
+    localStorage.setItem("siteArray", auxSiteArray);
 
     console.log(localStorage.nomeArray,localStorage.emailArray,localStorage.telefoneArray);
     document.write("Informações salvas e impressas com sucesso!<br>"+"Nome: "+localStorage.nomeArray+"<br>Email: "+localStorage.emailArray+"<br>Telefone: "+localStorage.telefoneArray);
